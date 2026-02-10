fb0a98966c210eecbbeeb538024811fe7a616f63
Søren Reinke <soren@reinke.dk>
2026-02-10 09:58:09 +0100

    fix: correct NO_BROWSE condition in LP create script

--- LearningPath/09-Events/create.sh
+++ LearningPath/09-Events/create.sh
@@ -169,7 +169,7 @@ az webapp restart \
   --resource-group "$resource_group_name" \
   --name "$web_app_name"
 
-if [[ "-e" != "1" ]]; then
+if [[ "${NO_BROWSE:-0}" != "1" ]]; then
 az webapp browse \
   --resource-group "$resource_group_name" \
   --name "$web_app_name"
